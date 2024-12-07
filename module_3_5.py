cd4abf307c351a7bc238be2638b5a90bdadc790c
Олег <48029194+EnergFox@users.noreply.github.com>
2024-12-08 01:04:23 +0500

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -3,9 +3,9 @@ def get_multiplied_digits(number):
     first = int(str_number[0])
     if str_number[-1] == '0':
         str_number = str_number [ : -1]
-    if len(str_number) == 1:
+    if len(str_number) <= 1:
         return first
-    else:
+    if len(str_number) >= 1:
         return first * get_multiplied_digits(int(str_number[1:]))
 result = get_multiplied_digits(40203)
 print(result)
